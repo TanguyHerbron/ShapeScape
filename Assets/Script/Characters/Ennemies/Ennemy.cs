8427a5702a29ef26d6ecd827d9207a1f69199048
Romain <romain.rousseau5@gmail.com>
2019-11-05 15:42:44 +0100

    [ADD] Ennemy class

---
+++ Assets/Script/Characters/Ennemies/Ennemy.cs
@@ -0,0 +1,13 @@
+﻿namespace Assets.Characters.Ennemies
+{
+    public class Ennemy : Character
+    {
+        // Start is called before the first frame update
+        public Ennemy(string ennemyName, float ennemyHP)
+        {
+            this.Name = ennemyName;
+            this.HP = ennemyHP;
+        }
+    }
+}
+
