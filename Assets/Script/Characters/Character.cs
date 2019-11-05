6b32eb2f5e60acfb3dbe06b07f5861956be3ac66
Romain <romain.rousseau5@gmail.com>
2019-11-05 14:45:26 +0100

    [ADD] Character class

---
+++ Assets/Script/Characters/Character.cs
@@ -0,0 +1,147 @@
+﻿namespace Assets.Characters
+{
+    /// <summary>
+    /// Abstract class for every characters in the game
+    /// </summary>
+    public abstract class Character
+    {
+        private int id;
+        private float hp;
+        private float speed;
+        private bool invicible = false;
+        private bool damaged = false;
+
+        /***
+            GETTERS AND SETTERS
+        ***/
+
+        public int ID
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public float HP
+        {
+            get
+            {
+                return hp;
+            }
+
+            set
+            {
+                hp = value;
+            }
+        }
+
+        public float Speed
+        {
+            get
+            {
+                return speed;
+            }
+
+            set
+            {
+                speed= value;
+            }
+        }
+
+        public bool Invicible
+        {
+            get
+            {
+                return invicible;
+            }
+
+            set
+            {
+                invicible = value;
+            }
+        }
+
+        public bool Damaged
+        {
+            get
+            {
+                return damaged;
+            }
+
+            set
+            {
+                damaged= value;
+            }
+        }
+
+        /// <summary>
+        /// Checks if the character is dead
+        /// </summary>
+        /// <returns>boolean</returns>
+        public bool IsDead()
+        {
+            if ( hp <= 0 )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Reduces health of a character by one
+        /// </summary>
+        public void ApplyDamage(float amount)
+        {
+            if ( !invicible )
+            {
+                damaged = true;
+                hp  -= amount;
+            }
+        }
+
+        /// <summary>
+        /// Instantly add the amount to the character hp
+        /// </summary>
+        public void Heal(float amount)
+        {
+            hp  += amount;
+        }
+
+        /// <summary>
+        /// Adds the amount of hp to the character over time
+        /// </summary>
+        public void RegenerateHealth(float dot, float amount)
+        {
+            hp  += amount;
+        }
+
+        /// <summary>
+        /// Reduces the amount of hp to the character over time
+        /// </summary>
+        public void Bleed(float dot, float amount)
+        {
+            hp  -= amount;
+        }
+
+        /// <summary>
+        /// Attack method
+        /// </summary>
+        public void Attack(float damage, Character target)
+        {
+            target.HP  -= damage;
+        }
+
+
+
+
+    }
+}
