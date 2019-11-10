6198b905c6e833aed76ceb4a326f503596572637
Romain <romain.rousseau5@gmail.com>
2019-11-10 17:37:44 +0100

    [ADD] Player Object

---
+++ Assets/Script/Characters/Player.cs
@@ -0,0 +1,12 @@
+﻿namespace Assets.Characters
+{
+    public class Player : Character
+    {
+        public Player(string playerName, float playerHP)
+        {
+            this.Name = playerName;
+            this.HP = playerHP;
+        }
+    }
+}
+
