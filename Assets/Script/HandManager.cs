cca45cc3a09881d39bc2332243ba07c2079bb88a
THerbron <tanguy.herbron@outlook.com>
2019-11-12 17:16:50 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Script/HandManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandManager : MonoBehaviour
+{
+    public GameObject weapon;
+    private GameObject oldWeapon;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(weapon != oldWeapon)
+        {
+            weapon = Instantiate(weapon, transform.position + weapon.transform.position, weapon.transform.rotation);
+            weapon.transform.parent = transform;
+            Destroy(oldWeapon);
+
+            oldWeapon = weapon;
+        }
+    }
+
+    public void attack()
+    {
+        Animator animator = weapon.GetComponent<Animator>();
+
+        animator.SetBool("Swing", true);
+    }
+}
