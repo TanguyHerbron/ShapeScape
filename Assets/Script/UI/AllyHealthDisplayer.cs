33777c602df1b00cb348023c83bfa8a71812b33b
Romain <romain.rousseau5@gmail.com>
2019-12-03 18:38:53 +0100

    [ADD] Ally Health Display

--- Assets/Script/UI/AllyHealthDisplayer.cs
+++ Assets/Script/UI/AllyHealthDisplayer.cs
@@ -1,18 +1,34 @@
-﻿using System.Collections;
+﻿using Assets.Entities;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AllyHealthDisplayer : MonoBehaviour
 {
+    private Character ally;
+    private SpriteRenderer spriteRenderer;
+
+    public Sprite[] allySprites;
+
+    private float currentHealth;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ally = this.GetComponent<Coward>();
+        currentHealth = ally.HP;
+
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if( currentHealth > ally.HP )
+        {
+            spriteRenderer.sprite = allySprites[(int) ally.HP - 1];
+            currentHealth = ally.HP;
+            Debug.Log(ally.HP);
+        }
     }
 }
