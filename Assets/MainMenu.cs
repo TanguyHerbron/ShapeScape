c6d2e626a4ee005debd33a05b335961f6f424044
Romain <romain.rousseau5@gmail.com>
2019-12-15 17:29:58 +0100

    [ADD] SImple Menus

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Update is called once per frame
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Generation_Tester");
+    }
+}
