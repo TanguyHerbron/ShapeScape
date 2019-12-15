cccaf8f0311fd59b5445fd3acea428a863177426
THerbron <tanguy.herbron@outlook.com>
2019-12-15 15:21:35 +0100

    [FIX] Ennemies spawning in walls

--- Assets/Script/RoomSpawn.cs
+++ Assets/Script/RoomSpawn.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class RoomSpawn : MonoBehaviour
 {
@@ -10,20 +11,30 @@ public class RoomSpawn : MonoBehaviour
     public int minEnnemies; // Minimum of ennemies to be spawned per room
     public int maxEnnemies; // Maximum of ennemies to be spawned per room
 
+    private Tilemap groundMap;
+
+    public void Start()
+    {
+        groundMap = GameObject.Find("Ground").GetComponent<Tilemap>();
+    }
+
     /// <summary>
     /// Spawn an ennemy at a random location inside the boundaries
     /// </summary>
     public void SpawnEnnemies(int xMin, int xMax, int yMin, int yMax)
     {
         int nbOfSpawn = Random.Range(minEnnemies, maxEnnemies + 1);
-        Vector2 spawnPos = new Vector2(0, 0);
         
         for(int i =0; i < nbOfSpawn; i++ )
         {
             int xPos = Random.Range(xMin, xMax);
             int yPos = Random.Range(yMin, yMax);
-            Vector3 pos = new Vector3(xPos*16, yPos*16, 0);
-            Instantiate(Ennemies[Random.Range(0,Ennemies.Length)], Camera.main.ScreenToWorldPoint(pos), new Quaternion());
+
+            if(groundMap.GetTile(new Vector3Int(xPos, yPos, 0)) != null)
+            {
+                Vector3 pos = new Vector3(xPos, yPos, 0);
+                Instantiate(Ennemies[Random.Range(0, Ennemies.Length)], pos, new Quaternion());
+            }
         }
     }
 }
