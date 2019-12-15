d15e1305800b3a7602bcd490e2c506e1013a0a39
Romain <romain.rousseau5@gmail.com>
2019-12-15 18:01:04 +0100

    [CHANGE] Sorting Scripts

---
+++ Assets/Script/UI/In Game/Pause.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("escape")){
+            PauseGame();
+        }
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1.0f;
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0.0f;
+    }
+}
