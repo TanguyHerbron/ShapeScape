18b492145325e0269473a55894ff9175233e220a
Romain <romain.rousseau5@gmail.com>
2019-12-15 18:09:21 +0100

    [ADD] Simple Pause Panel

--- Assets/Script/UI/In Game/Pause.cs
+++ Assets/Script/UI/In Game/Pause.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class Pause : MonoBehaviour
 {
+    public GameObject pausePanel;
+
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown("escape")){
             PauseGame();
+            pausePanel.SetActive(true);
         }
     }
 
     public void ResumeGame()
     {
         Time.timeScale = 1.0f;
+        pausePanel.SetActive(false);
     }
 
     public void PauseGame()
